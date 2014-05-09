79b05633c8926d8bac8e8cf16149fcd3d300d6e7
ZephireNZ <brynleymcdonald@gmail.com>
2014-05-09 13:13:36 +1200

    Update to new version of Vault - Adds new methods using OfflinePlayer, allowing for UUID methods - Makes name methods fallback to new methods via conversion

--- src/main/java/nz/co/noirland/bankofnoir/VaultConnector.java
+++ src/main/java/nz/co/noirland/bankofnoir/VaultConnector.java
@@ -1,9 +1,11 @@
 package nz.co.noirland.bankofnoir;
 
+import net.milkbowl.vault.economy.AbstractEconomy;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import nz.co.noirland.bankofnoir.config.PluginConfig;
 import nz.co.noirland.zephcore.Util;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.ServicesManager;
 
@@ -58,30 +60,45 @@ public class VaultConnector implements Economy {
     }
 
     @Override
+    public boolean hasAccount(OfflinePlayer player) {
+        return eco.hasBalance(player.getUniqueId());
+    }
+
+    @Override
     public boolean hasAccount(String player) {
-        return eco.hasBalance(Util.uuid(player));
+        return hasAccount(Util.player(player));
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer player) {
+        return eco.getBalance(player.getUniqueId());
     }
 
     @Override
     public double getBalance(String player) {
-        return eco.getBalance(Util.uuid(player));
+        return getBalance(Util.player(player));
+    }
+
+    @Override
+    public boolean has(OfflinePlayer player, double amount) {
+        return eco.getBalance(player.getUniqueId()) >= amount;
     }
 
     @Override
     public boolean has(String player, double amount) {
-        return eco.getBalance(Util.uuid(player)) >= amount;
+        return has(Util.player(player), amount);
     }
 
     @Override
-    public EconomyResponse withdrawPlayer(String player, double amount) {
-        double balance = eco.getBalance(Util.uuid(player));
+    public EconomyResponse withdrawPlayer(OfflinePlayer player, double amount) {
+        double balance = eco.getBalance(player.getUniqueId());
         boolean hasAmount = balance >= amount;
         EconomyResponse response;
 
         if(hasAmount) {
-            eco.setBalance(Util.uuid(player), balance - amount);
-            balance = eco.getBalance(Util.uuid(player));
-            response = new EconomyResponse(amount, eco.getBalance(Util.uuid(player)), EconomyResponse.ResponseType.SUCCESS, null);
+            eco.setBalance(player.getUniqueId(), balance - amount);
+            balance = eco.getBalance(player.getUniqueId());
+            response = new EconomyResponse(amount, balance, EconomyResponse.ResponseType.SUCCESS, null);
         }else{
             response = new EconomyResponse(0, balance, EconomyResponse.ResponseType.FAILURE, Strings.VAULT_INSUFFICIENT_FUNDS);
         }
@@ -89,18 +106,37 @@ public class VaultConnector implements Economy {
     }
 
     @Override
-    public EconomyResponse depositPlayer(String player, double amount) {
-        eco.setBalance(Util.uuid(player), eco.getBalance(Util.uuid(player)) + amount);
-        return new EconomyResponse(amount, eco.getBalance(Util.uuid(player)), EconomyResponse.ResponseType.SUCCESS, null);
+    public EconomyResponse withdrawPlayer(String player, double amount) {
+        return withdrawPlayer(Util.player(player), amount);
     }
 
     @Override
-    public boolean createPlayerAccount(String player) {
+    public EconomyResponse depositPlayer(OfflinePlayer player, double amount) {
+        eco.setBalance(player.getUniqueId(), eco.getBalance(player.getUniqueId()) + amount);
+        return new EconomyResponse(amount, eco.getBalance(player.getUniqueId()), EconomyResponse.ResponseType.SUCCESS, null);
+    }
+
+    @Override
+    public EconomyResponse depositPlayer(String player, double amount) {
+        return depositPlayer(Util.player(player), amount);
+    }
+
+    @Override
+    public boolean createPlayerAccount(OfflinePlayer player) {
         return hasAccount(player);
     }
 
     @Override
+    public boolean createPlayerAccount(String player) {
+        return createPlayerAccount(Util.player(player));
+    }
+
+    @Override
     public EconomyResponse createBank(String name, String player) {
+        return getNotImplemented();
+    }
+
+    public EconomyResponse createBank(String name, OfflinePlayer player) {
         return getNotImplemented();
     }
 
@@ -135,7 +171,17 @@ public class VaultConnector implements Economy {
     }
 
     @Override
+    public EconomyResponse isBankOwner(String name, OfflinePlayer player) {
+        return getNotImplemented();
+    }
+
+    @Override
     public EconomyResponse isBankMember(String name, String player) {
+        return getNotImplemented();
+    }
+
+    @Override
+    public EconomyResponse isBankMember(String name, OfflinePlayer player) {
         return getNotImplemented();
     }
 
@@ -149,33 +195,62 @@ public class VaultConnector implements Economy {
     }
 
     @Override
-    public EconomyResponse withdrawPlayer(String player, String world, double amount) {
+    public EconomyResponse withdrawPlayer(OfflinePlayer player, String world, double amount) {
         return withdrawPlayer(player, amount);
     }
 
     @Override
-    public boolean hasAccount(String player, String world) {
+    public EconomyResponse withdrawPlayer(String player, String world, double amount) {
+        return withdrawPlayer(Util.player(player), world, amount);
+    }
+
+    @Override
+    public boolean hasAccount(OfflinePlayer player, String world) {
         return hasAccount(player);
     }
 
     @Override
-    public double getBalance(String player, String world) {
+    public boolean hasAccount(String player, String world) {
+        return hasAccount(Util.player(player));
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer player, String world) {
         return getBalance(player);
     }
 
     @Override
-    public boolean has(String player, String world, double amount) {
+    public double getBalance(String player, String world) {
+        return getBalance(Util.player(player), world);
+    }
+
+    @Override
+    public boolean has(OfflinePlayer player, String world, double amount) {
         return has(player, amount);
     }
 
     @Override
-    public EconomyResponse depositPlayer(String player, String world, double amount) {
+    public boolean has(String player, String world, double amount) {
+        return has(Util.player(player), world, amount);
+    }
+
+    @Override
+    public EconomyResponse depositPlayer(OfflinePlayer player, String world, double amount) {
         return depositPlayer(player, amount);
     }
 
     @Override
-    public boolean createPlayerAccount(String player, String world) {
+    public EconomyResponse depositPlayer(String player, String world, double amount) {
+        return depositPlayer(Util.player(player), world, amount);
+    }
+
+    @Override
+    public boolean createPlayerAccount(OfflinePlayer player, String world) {
         return createPlayerAccount(player);
     }
 
+    @Override
+    public boolean createPlayerAccount(String player, String world) {
+        return createPlayerAccount(Util.player(player), world);
+    }
 }
